8c07287975efeedc2abc52ce00a0dd724b0b4ec3
Víctor Reyes <unhill@gmail.com>
2024-11-01 23:06:51 +0100

    Refactor: make Flashcard schema strict which will prevent extra properties

--- backend/src/flashcards/types.ts
+++ backend/src/flashcards/types.ts
@@ -1,9 +1,11 @@
 import z from "zod";
 
-export const Flashcard = z.object({
-  id: z.string().uuid(),
-  question: z.string().min(1),
-  answer: z.string().min(1),
-});
+export const Flashcard = z
+  .object({
+    id: z.string().uuid(),
+    question: z.string().min(1),
+    answer: z.string().min(1),
+  })
+  .strict();
 
 export type Flashcard = z.infer<typeof Flashcard>;

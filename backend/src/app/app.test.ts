1eb1038795d4a2adb0d8a48a180fd5f6bde9a764
Víctor Reyes <unhill@gmail.com>
2024-11-01 21:16:25 +0100

    Test POST api/flashcards adds a flashcard

--- backend/src/app/app.test.ts
+++ backend/src/app/app.test.ts
@@ -2,6 +2,7 @@ import test from "node:test";
 import { deepEqual } from "node:assert/strict";
 import request from "supertest";
 import { creatApp } from ".";
+import { Flashcard } from "../flashcards";
 
 test("App is up and running", async () => {
   const app = creatApp();
@@ -16,4 +17,21 @@ test("GET api/flashcards returns empty array of flashcards", async () => {
 
   deepEqual(response.status, 200);
   deepEqual(response.body, []);
+});
+
+test("POST api/flashcards adds a flashcard", async () => {
+  const app = creatApp();
+
+  const postFlashcard = { question: "What's up?", answer: "I'm fine!" };
+  const response = await request(app)
+    .post("/api/flashcards")
+    .send(postFlashcard);
+
+  const flashcards = (await request(app).get("/api/flashcards")).body;
+  const flashcard = flashcards[0];
+
+  deepEqual(response.status, 201);
+  deepEqual(flashcards.length, 1);
+  deepEqual(flashcard.question, postFlashcard.question);
+  deepEqual(flashcard.answer, postFlashcard.answer);
 });

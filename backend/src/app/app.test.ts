b310d51be72176a96bada8d02daf1fbc9b294a0e
Víctor Reyes <unhill@gmail.com>
2024-11-01 20:22:32 +0100

    Create test checking GET api/flashcards returns empty array

--- backend/src/app/app.test.ts
+++ backend/src/app/app.test.ts
@@ -9,3 +9,11 @@ test("App is up and running", async () => {
 
   deepEqual(response.status, 200);
 });
+
+test("GET api/flashcards returns empty array of flashcards", async () => {
+  const app = creatApp();
+  const response = await request(app).get("/api/flashcards");
+
+  deepEqual(response.status, 200);
+  deepEqual(response.body, []);
+});

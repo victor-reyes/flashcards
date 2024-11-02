bf8687efc5c664365505222c43a75fee058bbdd6
Víctor Reyes <unhill@gmail.com>
2024-11-02 21:37:46 +0100

    Add User schema and type with zod

---
+++ backend/src/users/types.ts
@@ -0,0 +1,11 @@
+import z from "zod";
+
+export const User = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  username: z.string().min(3),
+});
+
+export type User = z.infer<typeof User>;
+
+export type Db = Set<User>;

54d22094adb1f84150d066df67b0525e9278a904
Víctor Reyes <unhill@gmail.com>
2024-11-02 16:00:26 +0100

    Set up test database and change tests accordingly

--- backend/src/app/app.test.ts
+++ backend/src/app/app.test.ts
@@ -2,50 +2,52 @@ import test, { beforeEach } from "node:test";
 import { deepEqual } from "node:assert/strict";
 import request from "supertest";
 import { creatApp } from ".";
-import { createRepository, createRouter, Flashcard } from "../flashcards";
+import { createRepository, createRouter } from "../flashcards";
+import { App } from "supertest/types";
 
-function setUpApp(db: Array<Flashcard>) {
-  const flashcardsRepository = createRepository(db);
+let app: App;
+const TEST_FLASHCARD = {
+  id: "test_id",
+  question: "Capital of Great Britain",
+  answer: "London",
+};
+const TEST_DB = [{ ...TEST_FLASHCARD }];
+
+beforeEach(async () => {
+  const flashcardsRepository = createRepository([...TEST_DB]);
   const flashcardsRouter = createRouter(flashcardsRepository);
-  const app = creatApp(flashcardsRouter);
-  return app;
-}
+  app = creatApp(flashcardsRouter);
+});
 
 test("App is up and running", async () => {
-  const app = setUpApp([]);
   const response = await request(app).get("/");
 
   deepEqual(response.status, 200);
 });
 
-test("GET api/flashcards returns empty array of flashcards", async () => {
-  const app = setUpApp([]);
+test("GET api/flashcards returns default array of flashcards", async () => {
   const response = await request(app).get("/api/flashcards");
 
   deepEqual(response.status, 200);
-  deepEqual(response.body, []);
+  deepEqual(response.body, TEST_DB);
 });
 
 test("POST api/flashcards adds a flashcard", async () => {
-  const app = setUpApp([]);
-
   const postFlashcard = { question: "What's up?", answer: "I'm fine!" };
   const response = await request(app)
     .post("/api/flashcards")
     .send(postFlashcard);
 
   const flashcards = (await request(app).get("/api/flashcards")).body;
-  const flashcard = flashcards[0];
+  const flashcard = flashcards[1];
 
   deepEqual(response.status, 201);
-  deepEqual(flashcards.length, 1);
+  deepEqual(flashcards.length, 2);
   deepEqual(flashcard.question, postFlashcard.question);
   deepEqual(flashcard.answer, postFlashcard.answer);
 });
 
 test("POST api/flashcards returns 400 if invalid body provided", async () => {
-  const app = setUpApp([]);
-
   const postFlashcard = { question: "What's up?" };
   const response = await request(app)
     .post("/api/flashcards")

c2bebe1329b1bcd39ab9374c82ab50ca735b4a09
Víctor Reyes <unhill@gmail.com>
2024-11-02 23:02:51 +0100

    Refactor: rename isUserAlreadyExist to doesUserAlreadyExist

--- backend/src/users/repository.ts
+++ backend/src/users/repository.ts
@@ -3,18 +3,18 @@ import { Db, Repository, User } from ".";
 export function createUserRepository(db: Db): Repository {
   return {
     async register(user: User): Promise<boolean> {
-      if (isUserAlreadyExist(db, user)) return false;
+      if (doesUserAlreadyExist(db, user)) return false;
       db.push(user);
       return true;
     },
 
     async login(user: User) {
-      if (isUserAlreadyExist(db, user)) return "welcome";
+      if (doesUserAlreadyExist(db, user)) return "welcome";
     },
   };
 }
 
-function isUserAlreadyExist(users: User[], newUser: User): boolean {
+function doesUserAlreadyExist(users: User[], newUser: User): boolean {
   return (
     users.find(
       (user) =>

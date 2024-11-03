4b7af6ea7de1fa249237f41e89e942182df74f25
Víctor Reyes <unhill@gmail.com>
2024-11-03 11:50:36 +0100

    Refactor: switch to Service, rename Service's methods and simplify router by removing business logic

--- backend/src/flashcards/router.ts
+++ backend/src/flashcards/router.ts
@@ -1,47 +1,39 @@
 import { Router } from "express";
-import { Flashcard, PatchedFlashcard } from ".";
-import { v4 } from "uuid";
+import { Flashcard } from ".";
 
-export type Repository = {
+export type Service = {
   getAll: () => Promise<Flashcard[]>;
-  getBy: (id: string) => Promise<Flashcard | undefined>;
-  save: (flashcard: Flashcard) => Promise<void>;
-  patch: (id: string, patchedFlashcard: PatchedFlashcard) => Promise<Boolean>;
-  deleteBy: (id: string) => Promise<boolean>;
+  getById: (id: string) => Promise<Flashcard | undefined>;
+  create: (flashcard: Flashcard) => Promise<boolean>;
+  update: (id: string, updatedFlashcard: Flashcard) => Promise<boolean>;
+  deleteById: (id: string) => Promise<boolean>;
 };
 
-export function createFlashcardRouter(repository: Repository) {
+export function createFlashcardRouter(service: Service) {
   const router = Router();
 
   router.get("/", async (req, res) => {
-    res.json(await repository.getAll());
+    res.json(await service.getAll());
   });
 
   router.get("/:id", async (req, res) => {
-    const id = req.params.id;
-    const flaschard = await repository.getBy(id);
+    const flaschard = await service.getById(req.params.id);
     if (flaschard) res.json(flaschard);
     else res.sendStatus(404);
   });
 
   router.post("/", async (req, res) => {
-    const id = v4();
-    const flashcard = Flashcard.parse({ id, ...req.body });
-    await repository.save(flashcard);
+    await service.create(req.body);
     res.sendStatus(201);
   });
 
   router.patch("/:id", async (req, res) => {
-    const id = req.params.id;
-    const patchedFlashcard = PatchedFlashcard.parse(req.body);
-
-    if (await repository.patch(id, patchedFlashcard)) res.sendStatus(204);
+    if (await service.update(req.params.id, req.body)) res.sendStatus(204);
     else res.sendStatus(404);
   });
 
   router.delete("/:id", async (req, res) => {
-    const id = req.params.id;
-    if (await repository.deleteBy(id)) res.sendStatus(204);
+    if (await service.deleteById(req.params.id)) res.sendStatus(204);
     else res.sendStatus(404);
   });
 

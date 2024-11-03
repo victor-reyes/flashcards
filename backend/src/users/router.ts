59665ad226bd7420fb494065aa6dbd956d691fc9
Víctor Reyes <unhill@gmail.com>
2024-11-03 11:21:58 +0100

    Make User Router accept argument of type service and remove business logic from it

--- backend/src/users/router.ts
+++ backend/src/users/router.ts
@@ -1,24 +1,22 @@
 import { Router } from "express";
 import { User } from "./types";
 
-export type Repository = {
+export type Service = {
   register: (user: User) => Promise<boolean>;
   login: (user: User) => Promise<string | undefined>;
 };
 
-export function createUserRouter(repository: Repository) {
+export function createUserRouter(service: Service) {
   const router = Router();
 
   router.post("/register", async (req, res) => {
-    const user = User.parse(req.body);
-    (await repository.register(user))
+    (await service.register(req.body))
       ? res.sendStatus(201)
       : res.sendStatus(409);
   });
 
   router.post("/login", async (req, res) => {
-    const user = User.parse(req.body);
-    const accessToken = await repository.login(user);
+    const accessToken = await service.login(req.body);
     accessToken ? res.status(200).json({ accessToken }) : res.sendStatus(404);
   });
 
